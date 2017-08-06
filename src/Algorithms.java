94c9068c227a4a07bcda56d98b6a49f923257404
Anthony Klobas <atklobas@gmail.com>
2017-08-06 00:16:16 -0700

    implemented some sorts

---
+++ src/Algorithms.java
@@ -0,0 +1,197 @@
+import java.util.Arrays;
+import java.util.Random;
+
+import collections.PriorityQueue;
+
+public class Algorithms {
+	private static Random rand= new Random();
+	/**
+	 * This implementation:
+	 * Runs in O(n log(n)) time and O(n) space
+	 * 
+	 * This algorithm is stable (if 2 components have the same 
+	 * organizational value, they retain their respective order)
+	 * 
+	 * @param toSort array to be sorted
+	 */
+	public static void mergeSort(int[] toSort){
+		mergeHelper(0,toSort.length,Arrays.copyOf(toSort, toSort.length),toSort);
+	}
+	private static void mergeHelper(int start,int end,int[] source,int[] dest){
+		if(end-start>1){
+			int mid=(start+end)/2;
+			mergeHelper(start,mid,dest,source);
+			mergeHelper(mid,end,dest,source);
+			for(int i=start,i1=start,i2=mid;i<end;i++){
+				if(i2>=end||(i1<mid &&source[i1]<=source[i2])){
+					dest[i]=source[i1];
+					i1++;
+				}else{
+					dest[i]=source[i2];
+					i2++;
+				}
+			}
+			
+		}
+	}
+	
+	/**
+	 * This implementation:
+	 * Runs in O(n log(n)) average time, O(n^2) time in worst case
+	 * Runs in O(log(n)) average space, O(n) space in worst case
+	 * space requirement is because of stack size
+	 * 
+	 * This algorithm is randomized, so 2 on the same array may
+	 * take different amounts of time to complete
+	 * 
+	 * This algorithm is unstable
+	 * 
+	 * 
+	 * @param toSort array to be sorted
+	 */
+	public static void quickSort(int[] toSort){
+		
+		QuickSortHelper(toSort,0,toSort.length);
+		
+	}
+	//coming from outside edges should split large blocks of "same" in half 
+	private static void QuickSortHelper(int[] toSort,int start,int end){
+		if(end-start>1){
+			
+			int pivot=toSort[start+rand.nextInt(end-start)];
+			
+			int i=start;
+			int j=end-1;
+			while(j>=i){
+				if(toSort[i]<pivot){
+					i++;
+				}else if(toSort[j]>pivot){
+					j--;
+				}else{
+					int swap=toSort[i];
+					toSort[i]=toSort[j];
+					toSort[j]=swap;
+					i++;
+					j--;
+				}
+
+			}
+			if(j>start)QuickSortHelper(toSort,start,j+1);
+			if(i<end)QuickSortHelper(toSort,i,end);
+		}
+
+		
+	}
+	
+	//if you have a list of the same elements it's O(n^2)
+	private static void depQuickSortHelper(int[] toSort,int start,int end){
+		if(end-start>1){
+			
+			int pivotIndex=(start+end)/2;//rand.nextInt(end-start);
+			int pivot=toSort[pivotIndex];
+			
+			toSort[pivotIndex]=toSort[start];
+			toSort[start]=pivot;
+			int split=start+1;
+			for(int i=split;i<end;i++){
+				if(toSort[i]<pivot){
+				int swap=toSort[split];
+				toSort[split]=toSort[i];
+				toSort[i]=swap;
+				split++;
+				}
+			}
+			
+			toSort[start]=toSort[split-1];
+			toSort[split-1]=pivot;
+			depQuickSortHelper(toSort,start,split-1);
+			depQuickSortHelper(toSort,split,end);
+		}
+
+		
+	}
+	
+	/**
+	 * This implementation:
+	 * Runs in O(n log(n)) time and O(log(n)) space
+	 * 
+	 * I've never implemented Heapsort before, so doing it
+	 * in place (for that sweet space efficiency) should be fun
+	 * 
+	 * This algorithm is unstable
+	 * 
+	 * This algorithm supposedly has larger constant factors 
+	 * and is more likely to cause cache misses than quicksort
+	 * 
+	 * @param toSort array to be sorted
+	 */
+	public static void heapSort(int[] toSort){
+		//this is just test code, since i already have a heap implemenation
+		
+		
+		//my heap appears about half the speed of java.util, which could be explained by my adding being nlogn not n
+		//java.util.PriorityQueue<Integer >heap=new java.util.PriorityQueue<Integer >();
+		
+		PriorityQueue<Integer> heap=new PriorityQueue<Integer>(10485762*3);
+		for(int i:toSort){
+			heap.add(i);
+		}
+		for(int i=0;!heap.isEmpty();i++){
+			toSort[i]=heap.poll();
+		}
+		
+	}
+	
+	
+	
+	/**
+	 * This implementation:
+	 * Runs in O(n^2) time and O(l) space
+	 * 
+	 * famously slow, though every swap is adjacent
+	 * which might be useful for something
+	 * 
+	 * This algorithm is stable
+	 * 
+	 * @param toSort array to be sorted
+	 */
+	public static void bubbleSort(int[] toSort){
+		
+	}
+	
+	/**
+	 * This implementation:
+	 * Runs in O(n^2) time and O(l) space
+	 * 
+	 * If you asked a random intro to cs student to
+	 * write a sorting algorithm you'd get this is
+	 * likely what you'd get
+	 * 
+	 * This algorithm is unstable
+	 * 
+	 * @param toSort array to be sorted
+	 */
+	public static void selectionSort(int[] toSort){
+		
+	}
+	
+	/**
+	 * This implementation:
+	 * Runs in O(n^2) time and O(l) space
+	 * 
+	 * Considered one of the "better" n^2 sorts.
+	 * This takes some advantage of already sorted
+	 * data and is used in some libraries "stableSort"
+	 * methods with merge sort for the lists/recurive
+	 * calls smaller than some n (better constants)
+	 * 
+	 * This algorithm is stable
+	 * 
+	 * @param toSort array to be sorted
+	 */
+	public static void insertionSort(int[] toSort){
+		
+	}
+	
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Random;
 import java.util.Stack;
 
@@ -6,9 +7,48 @@ import collections.ArrayStack;
 import collections.PriorityQueue;
 
 public class Main {
-	public static final int TEST_ELEMENTS=21;
+	public static final int TEST_ELEMENTS=10;
+	public static final int TEST_ELEMENT__MAX_SIZE=41943040;
+	private static Random rand= new Random();
 	public static void main(String[] args){
 		
+		
+		for(int i=0,elements=10;i<23;i++,elements*=2){
+		
+		int[] testArray=generateRandom(elements);
+		//System.out.println(Arrays.toString(testArray));
+		long startTime= System.currentTimeMillis();
+		Algorithms.quickSort(testArray);
+		long endTime= System.currentTimeMillis();
+		//System.out.println(Arrays.toString(testArray));
+		System.out.println("QS test of "+elements+" elements took "+(endTime-startTime)+" milis");
+		testArray=generateRandom(elements);
+		//System.out.println(Arrays.toString(testArray));
+		startTime= System.currentTimeMillis();
+		Algorithms.mergeSort(testArray);
+		endTime= System.currentTimeMillis();
+		//System.out.println(Arrays.toString(testArray));
+		System.out.println("MS test of "+elements+" elements took "+(endTime-startTime)+" milis");
+		
+		}
+		
+	}
+	
+	public static int[] generateRandom(int length){
+		int[] returnArray=new int[length];
+		for(int i=0;i<length;i++){
+			returnArray[i]=rand.nextInt(TEST_ELEMENT__MAX_SIZE);
+		}
+		return returnArray;
+	}
+	
+	
+	
+	
+	
+	
+	public static void testStack(){
+
 		/*/Stack<Object> stack =new Stack<Object>();
 		/*/ArrayStack<Object> stack =new ArrayStack<Object>();/**/
 		///stack.push(1);
@@ -39,6 +79,7 @@ public class Main {
 	
 	
 	
+	
 	/* This is not a particularly good heap sort as it's space complexity is O(n) 
 	 * 
 	 * psudocode for heapsort with space complexity O(1) (if done on an array):
@@ -52,11 +93,11 @@ public class Main {
 	 */
 	public static void testHeapSort(){
 		PriorityQueue<Integer> heap=new PriorityQueue<Integer>();
-		Random rand= new Random();
+		
 		
 		//Add random elements to structure, and print out like a list;
 		for(int i=0;i<TEST_ELEMENTS;i++){
-			int toadd=rand.nextInt(200);
+			int toadd=rand.nextInt(TEST_ELEMENT__MAX_SIZE);
 			heap.add(toadd);
 			if(i>0){
 				System.out.print(", ");
